9c499fd1df157294205d545321cb817bf51441c8
rem <ahmedelsherif.osama@gmail.com>
2026-04-22 14:32:30 +0200

    chore: removed useless comments from predict

--- src/inference/predict.py
+++ src/inference/predict.py
@@ -5,17 +5,8 @@ import pandas as pd
 import argparse
 
 
-# -----------------
-# PATH CONFIG
-# -----------------
-
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 ARTIFACTS_DIR = os.path.join(BASE_DIR, "artifacts")
-
-
-# -----------------
-# LOAD ARTIFACTS
-# -----------------
 
 
 def load_latest_model():
@@ -46,11 +37,6 @@ def load_scaler():
     return scaler
 
 
-# -----------------
-# PREPROCESS INPUT
-# -----------------
-
-
 def preprocess_input(df: pd.DataFrame):
     """
     Apply the same preprocessing used during training.
@@ -61,11 +47,6 @@ def preprocess_input(df: pd.DataFrame):
         df["Amount"] = scaler.transform(df[["Amount"]])
 
     return df
-
-
-# -----------------
-# PREDICTION
-# -----------------
 
 
 def predict(transaction_df: pd.DataFrame, threshold: float = 0.5):
@@ -82,10 +63,8 @@ def predict(transaction_df: pd.DataFrame, threshold: float = 0.5):
 
     model = load_latest_model()
 
-    # preprocess features
     transaction_df = preprocess_input(transaction_df)
 
-    # prediction
     fraud_proba = model.predict_proba(transaction_df)[:, 1]
     fraud_pred = (fraud_proba >= threshold).astype(int)
 
@@ -97,11 +76,6 @@ def predict(transaction_df: pd.DataFrame, threshold: float = 0.5):
         )
 
     return results
-
-
-# -----------------
-# ENTRY POINT
-# -----------------
 
 
 def run_cli():
@@ -123,7 +97,6 @@ def run_cli():
 
     args = parser.parse_args()
 
-    # load input data
     df = pd.read_csv(args.input)
 
     print(f"[INFO] Loaded input data: {df.shape}")
@@ -135,7 +108,6 @@ def run_cli():
     print("\nPredictions:")
     print(output_df.head())
 
-    # optional save
     output_path = args.input.replace(".csv", "_predictions.csv")
     output_df.to_csv(output_path, index=False)
 
@@ -144,50 +116,6 @@ def run_cli():
 
 def main():
     run_cli()
-    # """
-    # Example local test.
-    # """
-    # # dummy example transaction
-    # sample = 
-{
-    "Time": 10000,
-    "V1": -1.359807,
-    "V2": -0.072781,
-    "V3": 2.536346,
-    "V4": 1.378155,
-    "V5": -0.338321,
-    "V6": 0.462388,
-    "V7": 0.239599,
-    "V8": 0.098698,
-    "V9": 0.363787,
-    "V10": 0.090794,
-    "V11": -0.551600,
-    "V12": -0.617801,
-    "V13": -0.991390,
-    "V14": -0.311169,
-    "V15": 1.468177,
-    "V16": -0.470401,
-    "V17": 0.207971,
-    "V18": 0.025791,
-    "V19": 0.403993,
-    "V20": 0.251412,
-    "V21": -0.018307,
-    "V22": 0.277838,
-    "V23": -0.110474,
-    "V24": 0.066928,
-    "V25": 0.128539,
-    "V26": -0.189115,
-    "V27": 0.133558,
-    "V28": -0.021053,
-    "Amount": 149.62,
-}
-
-    # df = pd.DataFrame([sample])
-
-    # result = predict(df)
-
-    # print("\nPrediction result:")
-    # print(result)
 
 
 if __name__ == "__main__":
